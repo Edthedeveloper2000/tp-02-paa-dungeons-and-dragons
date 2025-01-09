4432629671f2ba059d47a8eecc641d10c9e71c17
Edthedevelop2000 <edmarcosmendes2019@gmail.com>
2025-01-08 21:32:12 -0300

    feat: adiciona definição de cores

--- common.h
+++ common.h
@@ -7,4 +7,12 @@
 #include <string.h>
 #include <limits.h>
 
+// Definições de cores ANSI
+#define RED "\x1b[31m"       // Vermelho
+#define WHITE "\x1b[37m"     // Branco
+#define YELLOW "\x1B[33m"    // Amarelo
+#define BLUE "\x1B[34m"      // Azul
+#define GREEN "\e[0;32m"     // Verde
+#define RESET "\033[0m"      // Resetar cor
+
 #endif
